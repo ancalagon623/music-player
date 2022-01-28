6adb5faccf348388f41f535189497bf467f23ac9
Michael Grumbine <mgrumbine3@gmail.com>
2022-01-28 06:30:26 -0800

    added currentAlbum variable

--- scripts/album.js
+++ scripts/album.js
@@ -65,6 +65,8 @@ var createSongRow = function (songNumber, songName, songLength) {
 };
 
 var setCurrentAlbum = function(album) {
+  currentAlbum = album;
+
   var $albumTitle = $('.album-view-title');
   var $albumArtist = $('.album-view-artist');
   var $albumReleaseInfo = $('.album-view-release-info');
@@ -85,6 +87,7 @@ var setCurrentAlbum = function(album) {
 };
 
 var currentlyPlayingSongNumber = null;
+var currentAlbum = null;
 
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
